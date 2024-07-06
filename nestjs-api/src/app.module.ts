790b2755531ff790194fdecff0c0e76ab129fa81
DVasconcelos <di0g0vasconcelos@hotmail.com>
2024-07-06 18:28:45 -0300

    add dockerfile

--- nestjs-api/src/app.module.ts
+++ nestjs-api/src/app.module.ts
@@ -2,10 +2,29 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { TransactionsModule } from './transactions/transactions.module';
+// import { ConfigModule } from '@nestjs/config';
+// import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
-  imports: [TransactionsModule],
   controllers: [AppController],
+  imports: [
+    // ConfigModule.forRoot(),
+    // SequelizeModule.forRoot({
+    //   dialect: process.env.DB_CONNECTION as any,
+    //   host: process.env.DB_HOST,
+    //   port: parseInt(process.env.DB_PORT),
+    //   username: process.env.DB_USERNAME,
+    //   password: process.env.DB_PASSWORD,
+    //   database: process.env.DB_DATABASE,
+    //   models: [Transaction, Account, Report],
+    //   autoLoadModels: true,
+    //   synchronize: true,
+    //   sync: {
+    //     alter: true,
+    //   },
+    // }),
+    TransactionsModule,
+  ],
   providers: [AppService],
 })
 export class AppModule {}
